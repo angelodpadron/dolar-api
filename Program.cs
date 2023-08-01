cbfb77fc030124d4c9f48d104c38c1ad0cd15814
Angelo Padron <padron891@gmail.com>
2023-08-01 19:39:52 -0300

    refactors and other minor changes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-using System.Xml.Linq;
+using System.Text.Json;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-    options =>
+builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
@@ -32,7 +31,8 @@ var app = builder.Build();
 
 app.UseSwagger();
 
-const string CotizacionesUrl = "https://www.dolarsi.com/api/dolarSiInfo.xml";
+var cotizacionesUrl = builder.Configuration.GetValue<string>("CotizacionesUrl");
+var endpoints = await GetEndpointsFor("Common/Endpoints.json");
 
 // dolar
 
@@ -40,137 +40,20 @@ var dolar = app.MapGroup("/dolar");
 
 // generales
 
-dolar.MapGet("/principales", GetValoresPrincipales);
-dolar.MapGet("/oficial", (HttpClient httpClient) => GetValorDolar(httpClient, CotizacionesUrl, "Dolar Oficial"));
-dolar.MapGet("/blue", (HttpClient httpClient) => GetValorDolar(httpClient, CotizacionesUrl, "Dolar Blue"));
-dolar.MapGet("/bolsa", (HttpClient httpClient) => GetValorDolar(httpClient, CotizacionesUrl, "Dolar Bolsa"));
-dolar.MapGet("/liqui", (HttpClient httpClient) => GetValorDolar(httpClient, CotizacionesUrl, "Dolar Contado con Liqui"));
-dolar.MapGet("/turista", (HttpClient httpClient) => GetValorDolar(httpClient, CotizacionesUrl, "Dolar Turista"));
-dolar.MapGet("/soja", (HttpClient httpClient) => GetValorDolar(httpClient, CotizacionesUrl, "Dolar Soja"));
+dolar.MapGet("/principales", (HttpClient http) => CotizacionDolar.GetValoresPrincipales(http, cotizacionesUrl));
+
+// por tipo
+
+foreach (var tipo in endpoints["TipoDolar"])
+{
+    dolar.MapGet($"/{tipo.Value}", (HttpClient http) => CotizacionDolar.GetValorDolar(http, cotizacionesUrl, tipo.Key));
+}
 
 // oficial por banco
 
-dolar.MapGet("/oficial/banco_nacion", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Nación"));
-dolar.MapGet("/oficial/banco_hipotecario", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Hipotecario"));
-dolar.MapGet("/oficial/banco_bbva", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco BBVA"));
-dolar.MapGet("/oficial/banco_galicia", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Galicia"));
-dolar.MapGet("/oficial/banco_santander", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Santander"));
-dolar.MapGet("/oficial/banco_supervielle", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Supervielle"));
-dolar.MapGet("/oficial/banco_patagonia", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Patagonia"));
-dolar.MapGet("/oficial/banco_itau", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Itaú Argentina S.A."));
-dolar.MapGet("/oficial/banco_icbc", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Industrial and Commercial Bank of China "));
-dolar.MapGet("/oficial/banco_piano", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Piano"));
-dolar.MapGet("/oficial/nuevo_banco_del_chaco", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Nuevo Banco del Chaco"));
-dolar.MapGet("/oficial/banco_de_la_pampa", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco de la Pampa"));
-dolar.MapGet("/oficial/banco_roela", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Roela"));
-dolar.MapGet("/oficial/banco_de_cordoba", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco de Córdoba"));
-dolar.MapGet("/oficial/banco_ciudad_buenos_aires", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco de la Ciudad de Buenos Aires"));
-dolar.MapGet("/oficial/banco_provincia_buenos_aires", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco de la Provincia de Buenos Aires"));
-dolar.MapGet("/oficial/banco_de_formosa", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco de Formosa S.A."));
-dolar.MapGet("/oficial/banco_del_chubut", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco del Chubut S.A."));
-dolar.MapGet("/oficial/banco_provincia_tierra_del_fuego", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco de Tierra del Fuego"));
-dolar.MapGet("/oficial/banco_provincia_de_neuquen", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Provincia de Neuquén S.A."));
-dolar.MapGet("/oficial/banco_rioja_sau", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Rioja Sociedad Anónima Unipersonal"));
-dolar.MapGet("/oficial/banco_bica", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Bica S.A."));
-dolar.MapGet("/oficial/banco_julio", (HttpClient httpClient) => GetValorDolarOficialBanco(httpClient, CotizacionesUrl, "Banco Julio S.A."));
-
-static async Task<IResult> GetValoresPrincipales(HttpClient httpClient)
+foreach (var banco in endpoints["Bancos"])
 {
-    try
-    {
-        HttpResponseMessage responseMessage = await httpClient.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlString = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlString);
-
-        var cotizacionesDolares = xml.Descendants("Dolar").Elements()
-            .Select(c => new
-            {
-                Nombre = c.Element("nombre")?.Value,
-                Compra = c.Element("compra")?.Value,
-                Venta = c.Element("venta")?.Value,
-                Decimales = c.Element("decimales")?.Value
-            }
-            )
-            .ToList();
-
-        return TypedResults.Ok(cotizacionesDolares);
-
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
-
-}
-
-static async Task<IResult> GetValorDolar(HttpClient httpClient, string cotizacionesUrl, string nombreCotizacion)
-{
-    try
-    {
-        HttpResponseMessage responseMessage = await httpClient.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlString = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlString);
-
-        var dolar = xml
-            .Descendants("valores_principales")
-            .Elements()
-            .FirstOrDefault(c => c.Element("nombre")?.Value == nombreCotizacion);
-
-        if (dolar is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "valores_principales");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = dolar.Element("nombre")?.Value,
-            Compra = dolar.Element("compra")?.Value,
-            Venta = dolar.Element("venta")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
-}
-
-static async Task<IResult> GetValorDolarOficialBanco(HttpClient http, string cotizacionesUrl, string banco)
-{
-    try
-    {
-        HttpResponseMessage responseMessage = await http.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlDoc = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlDoc);
-
-        var dolar = xml
-            .Descendants("Capital_Federal")
-            .Elements()
-            .FirstOrDefault(c => c.Element("nombre")?.Value == banco); // con acento en la "o"
-
-        if (dolar is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "Capital_Federal");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = dolar.Element("nombre")?.Value,
-            Compra = dolar.Element("compra")?.Value,
-            Venta = dolar.Element("venta")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
+    dolar.MapGet($"/oficial/{banco.Value}", (HttpClient http) => CotizacionDolar.GetValorOficialBanco(http, cotizacionesUrl, banco.Key));
 }
 
 // reservas y circulante
@@ -178,216 +61,52 @@ static async Task<IResult> GetValorDolarOficialBanco(HttpClient http, string cot
 var reservas = app.MapGroup("/reservas");
 var circulante = app.MapGroup("/circulante");
 
-reservas.MapGet("/", (HttpClient http) => GetRyC(http, CotizacionesUrl, "Reservas"));
-circulante.MapGet("/", (HttpClient http) => GetRyC(http, CotizacionesUrl, "Circulante"));
-
-static async Task<IResult> GetRyC(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
-{
-    try
-    {
-        HttpResponseMessage responseMessage = await http.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlDoc = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlDoc);
-
-        var reservas = xml.
-            Descendants("Reservas_y_circulante")
-            .Elements()
-            .FirstOrDefault(q => q.Element("nombre")?.Value.Contains(nombreCotizacion) ?? false);
-
-
-        if (reservas is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "RESERVAS_Y_CIRCULANTE");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = reservas.Element("nombre")?.Value,
-            Valor = reservas.Element("compra")?.Value,
-            Obervaciones = reservas.Element("observaciones")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
-}
+reservas.MapGet("/", (HttpClient http) => ReservasYCirculantes.GetRyC(http, cotizacionesUrl, "Reservas"));
+circulante.MapGet("/", (HttpClient http) => ReservasYCirculantes.GetRyC(http, cotizacionesUrl, "Circulante"));
 
 // riesgo pais
 
 var riesgoPais = app.MapGroup("/riesgo_pais");
 
-riesgoPais.MapGet("/argentina", (HttpClient http) => GetRiesgoPais(http, CotizacionesUrl, "Argentina"));
-riesgoPais.MapGet("/brasil", (HttpClient http) => GetRiesgoPais(http, CotizacionesUrl, "Brasil"));
-riesgoPais.MapGet("/uruguay", (HttpClient http) => GetRiesgoPais(http, CotizacionesUrl, "Uruguay"));
-riesgoPais.MapGet("/mexico", (HttpClient http) => GetRiesgoPais(http, CotizacionesUrl, "México"));
-
-static async Task<IResult> GetRiesgoPais(HttpClient http, string cotizacionesUrl, string pais)
+foreach (var pais in endpoints["RiesgoPais"])
 {
-    try
-    {
-        HttpResponseMessage responseMessage = await http.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlDoc = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlDoc);
-
-        var riesgoPais = xml.
-            Descendants("Riesgo_pais")
-            .Elements()
-            .FirstOrDefault(q => q.Element("nombre")?.Value.Contains(pais) ?? false);
-
-        if (riesgoPais is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "RIESGO_PAIS");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = riesgoPais.Element("nombre")?.Value,
-            Puntos = riesgoPais.Element("compra")?.Value,
-            Variacion = riesgoPais.Element("venta")?.Value,
-            MejorCompra = riesgoPais.Element("mejor_compra")?.Value,
-            MejorVenta = riesgoPais.Element("mejor_venta")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
-
+    riesgoPais.MapGet($"/{pais.Value}", (HttpClient http) => RiesgoPais.GetRiesgoPais(http, cotizacionesUrl, pais.Key));
 }
 
 // agro
 
 var agro = app.MapGroup("/agro");
 
-agro.MapGet("/trigo", (HttpClient httpClient) => GetAgro(httpClient, CotizacionesUrl, "Trigo"));
-agro.MapGet("/maiz", (HttpClient httpClient) => GetAgro(httpClient, CotizacionesUrl, "Maiz"));
-agro.MapGet("/soja_rosario", (HttpClient httpClient) => GetAgro(httpClient, CotizacionesUrl, "Soja Rosario"));
-agro.MapGet("/soja_chicago", (HttpClient httpClient) => GetAgro(httpClient, CotizacionesUrl, "Soja Chicago"));
-
-static async Task<IResult> GetAgro(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+foreach (var producto in endpoints["Agro"])
 {
-    try
-    {
-        HttpResponseMessage responseMessage = await http.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlDoc = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlDoc);
-
-        var agro = xml.
-            Descendants("Agro")
-            .Elements()
-            .FirstOrDefault(q => q.Element("nombre")?.Value.Contains(nombreCotizacion) ?? false);
-
-        if (agro is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "AGRO");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = agro.Element("nombre")?.Value,
-            Compra = agro.Element("compra")?.Value,
-            Venta = agro.Element("venta")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
+    agro.MapGet($"/{producto.Value}", (HttpClient http) => Agro.GetAgro(http, cotizacionesUrl, producto.Key));
 }
 
 // energia
 
 var energia = app.MapGroup("/energia");
 
-energia.MapGet("/petroleo/wti", (HttpClient httpClient) => GetEnergia(httpClient, CotizacionesUrl, "WTI"));
-energia.MapGet("/petroleo/brent", (HttpClient httpClient) => GetEnergia(httpClient, CotizacionesUrl, "BRENT"));
-energia.MapGet("/gas", (HttpClient httpClient) => GetEnergia(httpClient, CotizacionesUrl, "Gas"));
-
-static async Task<IResult> GetEnergia(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+foreach (var e in endpoints["Energia"])
 {
-    try
-    {
-        HttpResponseMessage responseMessage = await http.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlDoc = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlDoc);
-
-        var energia = xml.
-            Descendants("Energia")
-            .Elements()
-            .FirstOrDefault(q => q.Element("nombre")?.Value.Contains(nombreCotizacion) ?? false);
-
-        if (energia is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "ENERGIA");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = energia.Element("nombre")?.Value,
-            Compra = energia.Element("compra")?.Value,
-            Venta = energia.Element("venta")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
+    energia.MapGet($"/{e.Value}", (HttpClient http) => Energia.GetEnergia(http, cotizacionesUrl, e.Key));
 }
+
 
 // metales
 
 var metales = app.MapGroup("/metales");
 
-metales.MapGet("/oro", (HttpClient httpClient) => GetMetales(httpClient, CotizacionesUrl, "Oro"));
-metales.MapGet("/plata", (HttpClient httpClient) => GetMetales(httpClient, CotizacionesUrl, "Plata"));
-metales.MapGet("/cobre", (HttpClient httpClient) => GetMetales(httpClient, CotizacionesUrl, "Cobre"));
-
-static async Task<IResult> GetMetales(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+foreach (var m in endpoints["Metales"])
 {
-    try
-    {
-        HttpResponseMessage responseMessage = await http.GetAsync(CotizacionesUrl);
-        responseMessage.EnsureSuccessStatusCode();
-        string xmlDoc = await responseMessage.Content.ReadAsStringAsync();
-
-        var xml = XDocument.Parse(xmlDoc);
-
-        var metales = xml.
-            Descendants("Metales")
-            .Elements()
-            .FirstOrDefault(q => q.Element("nombre")?.Value.Contains(nombreCotizacion) ?? false);
-
-        if (metales is null) return TypedResults.NotFound();
-
-        var actualizacion = xml.Descendants("ultima").Elements().FirstOrDefault(c => c.Element("nombre")?.Value == "METALES");
-
-        return TypedResults.Ok(new
-        {
-            Nombre = metales.Element("nombre")?.Value,
-            Compra = metales.Element("compra")?.Value,
-            Venta = metales.Element("venta")?.Value,
-            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
-        });
-
-    }
-    catch (HttpRequestException)
-    {
-        return TypedResults.StatusCode(503);
-    }
+    metales.MapGet($"/{m.Value}", (HttpClient http) => Metales.GetMetales(http, cotizacionesUrl, m.Key));
 }
+
+static async Task<Dictionary<string, Dictionary<string, string>>> GetEndpointsFor(string path)
+{
+    string json = await File.ReadAllTextAsync(path);
+    return JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(json);
+}
+
 
 app.UseSwaggerUI();
 
---
+++ Utils.cs
@@ -0,0 +1,198 @@
+using System.Xml.Linq;
+
+public static class Utils
+{
+    public static async Task<XDocument> GetXmlDoc(HttpClient httpClient, string cotizacionesUrl)
+    {
+        HttpResponseMessage responseMessage = await httpClient.GetAsync(cotizacionesUrl);
+        responseMessage.EnsureSuccessStatusCode();
+        return XDocument.Parse(await responseMessage.Content.ReadAsStringAsync());
+    }
+
+    public static XElement GetXElementFromNode(XDocument doc, string nodo, string nombre)
+    {
+        return doc.Descendants(nodo).Elements().FirstOrDefault(c => c.Element("nombre")?.Value.Contains(nombre) ?? false);
+    }
+}
+
+public static class CotizacionDolar
+{
+
+    public static async Task<IResult> GetValoresPrincipales(HttpClient httpClient, string cotizacionesUrl)
+    {
+        var doc = await Utils.GetXmlDoc(httpClient, cotizacionesUrl);
+
+        var cotizacionesDolares = doc.Descendants("Dolar").Elements()
+                .Select(c => new
+                {
+                    Nombre = c.Element("nombre")?.Value,
+                    Compra = c.Element("compra")?.Value,
+                    Venta = c.Element("venta")?.Value,
+                    Decimales = c.Element("decimales")?.Value
+                }
+                )
+                .ToList();
+
+        return TypedResults.Ok(cotizacionesDolares);
+
+    }
+    public static async Task<IResult> GetValorDolar(HttpClient httpClient, string cotizacionesUrl, string nombreCotizacion)
+    {
+        var doc = await Utils.GetXmlDoc(httpClient, cotizacionesUrl);
+        var dolar = Utils.GetXElementFromNode(doc, "valores_principales", nombreCotizacion);
+
+        if (dolar is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "valores_principales");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = dolar.Element("nombre")?.Value,
+            Compra = dolar.Element("compra")?.Value,
+            Venta = dolar.Element("venta")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+
+    }
+    public static async Task<IResult> GetValorOficialBanco(HttpClient http, string cotizacionesUrl, string banco)
+    {
+
+
+        var doc = await Utils.GetXmlDoc(http, cotizacionesUrl);
+        var dolar = Utils.GetXElementFromNode(doc, "Capital_Federal", banco);
+
+        if (dolar is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "Capital_Federal");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = dolar.Element("nombre")?.Value,
+            Compra = dolar.Element("compra")?.Value,
+            Venta = dolar.Element("venta")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+    }
+    
+}
+
+public static class ReservasYCirculantes
+{
+    public static async Task<IResult> GetRyC(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+    {
+
+        var doc = await Utils.GetXmlDoc(http, cotizacionesUrl);
+        var reservas = Utils.GetXElementFromNode(doc, "Reservas_y_circulante", nombreCotizacion);
+
+        if (reservas is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "RESERVAS_Y_CIRCULANTE");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = reservas.Element("nombre")?.Value,
+            Valor = reservas.Element("compra")?.Value,
+            Obervaciones = reservas.Element("observaciones")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+
+    }
+}
+
+public static class RiesgoPais
+{
+    public static async Task<IResult> GetRiesgoPais(HttpClient http, string cotizacionesUrl, string pais)
+    {
+
+        var doc = await Utils.GetXmlDoc(http, cotizacionesUrl);
+        var riesgoPais = Utils.GetXElementFromNode(doc, "Riesgo_pais", pais);
+
+        if (riesgoPais is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "RIESGO_PAIS");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = riesgoPais.Element("nombre")?.Value,
+            Puntos = riesgoPais.Element("compra")?.Value,
+            Variacion = riesgoPais.Element("venta")?.Value,
+            MejorCompra = riesgoPais.Element("mejor_compra")?.Value,
+            MejorVenta = riesgoPais.Element("mejor_venta")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+
+    }
+
+}
+
+public static class Agro
+{
+    public static async Task<IResult> GetAgro(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+    {
+
+        var doc = await Utils.GetXmlDoc(http, cotizacionesUrl);
+        var agro = Utils.GetXElementFromNode(doc, "Agro", nombreCotizacion);
+
+        if (agro is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "AGRO");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = agro.Element("nombre")?.Value,
+            Compra = agro.Element("compra")?.Value,
+            Venta = agro.Element("venta")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+
+    }
+}
+
+public static class Energia
+{
+    public static async Task<IResult> GetEnergia(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+    {
+
+        var doc = await Utils.GetXmlDoc(http, cotizacionesUrl);
+        var energia = Utils.GetXElementFromNode(doc, "Energia", nombreCotizacion);
+
+        if (energia is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "ENERGIA");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = energia.Element("nombre")?.Value,
+            Compra = energia.Element("compra")?.Value,
+            Venta = energia.Element("venta")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+
+    }
+}
+
+public static class Metales
+{
+    public static async Task<IResult> GetMetales(HttpClient http, string cotizacionesUrl, string nombreCotizacion)
+    {
+
+        var doc = await Utils.GetXmlDoc(http, cotizacionesUrl);
+        var metales = Utils.GetXElementFromNode(doc, "Metales", nombreCotizacion);
+
+        if (metales is null) return TypedResults.NotFound();
+
+        var actualizacion = Utils.GetXElementFromNode(doc, "ultima", "METALES");
+
+        return TypedResults.Ok(new
+        {
+            Nombre = metales.Element("nombre")?.Value,
+            Compra = metales.Element("compra")?.Value,
+            Venta = metales.Element("venta")?.Value,
+            Actualizado = $"{actualizacion?.Element("fecha")?.Value} {actualizacion?.Element("hora")?.Value}"
+        });
+
+    }
+}
+
+
+
